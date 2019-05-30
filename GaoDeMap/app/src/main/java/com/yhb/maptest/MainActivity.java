0dbdde251f73a9a743f1aadc3a598c002d4e1812
YeHaobo <17855366369@163.com>
2019-05-30 17:29:14 +0800

    高德地图Demo

--- GaoDeMap/app/src/main/java/com/yhb/maptest/MainActivity.java
+++ GaoDeMap/app/src/main/java/com/yhb/maptest/MainActivity.java
@@ -5,8 +5,10 @@ import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.BounceInterpolator;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.amap.api.maps.AMap;
@@ -36,6 +38,7 @@ public class MainActivity extends AppCompatActivity implements OnMyLocationChang
     private AMap aMap = null;
     private LatLonPoint mLatLonPoint = null;
     private GeocodeSearch mGeocodeSearch = null;
+    private Marker marker = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements OnMyLocationChang
         mUiSettings.setCompassEnabled(true);//设置指南针
         mUiSettings.setScaleControlsEnabled(true);//设置比例尺
         mUiSettings.setMyLocationButtonEnabled(true);//定位按钮
-        mUiSettings.setLogoPosition(AMapOptions.LOGO_POSITION_BOTTOM_CENTER);//logo位置
+        mUiSettings.setLogoPosition(AMapOptions.LOGO_POSITION_BOTTOM_LEFT);//logo位置
 
         aMap.setMyLocationStyle(myLocationStyle);//设置地图样式
         aMap.setOnMyLocationChangeListener(this);//设置定位回调
@@ -77,12 +80,13 @@ public class MainActivity extends AppCompatActivity implements OnMyLocationChang
     @Override
     public void onMyLocationChange(Location location) {
         if(mLatLonPoint == null){
-            //定位当前蓝点位置并且移动地图至中心点
+            //定位当前蓝点位置
             CameraUpdate mCameraUpdate = CameraUpdateFactory
                     .newCameraPosition(new CameraPosition(new LatLng(location.getLatitude(),location.getLongitude()),15,30,0));
+            //移动地图至中心点(无移动动画)
             aMap.moveCamera(mCameraUpdate);
-
-            addMyMark(location.getLatitude(),location.getLongitude());
+            //添加地标
+            marker = addMyMark(location.getLatitude(),location.getLongitude(),"当前位置","江苏省 南京市 建邺区 紫金西城1号楼");
         }
         //逆地理编码（坐标转地址）
         mLatLonPoint = new LatLonPoint(location.getLatitude(),location.getLongitude());
@@ -99,7 +103,8 @@ public class MainActivity extends AppCompatActivity implements OnMyLocationChang
             Toast.makeText(MainActivity.this,"回调地址失败，errCode:" + i,Toast.LENGTH_SHORT).show();
         }else{
             RegeocodeAddress mRegeocodeAddress = regeocodeResult.getRegeocodeAddress();
-            Log.i("GaoDeMap",mRegeocodeAddress.getFormatAddress());
+            String address = mRegeocodeAddress.getFormatAddress();
+            marker.setSnippet(address);
         }
     }
     @Override
@@ -108,32 +113,82 @@ public class MainActivity extends AppCompatActivity implements OnMyLocationChang
     /**
      * 添加Mark
      */
-    private void addMyMark(double latitude,double longitude){
-        //经纬度封装
-        LatLng latLng = new LatLng(latitude,longitude);
-        //将要显示的Mark视图
-        View view = View.inflate(this,R.layout.mark_image, null);
-        //自定义Mark
-        MarkerOptions markerOption = new MarkerOptions()
+    private Marker addMyMark(double latitude,double longitude,String title,String desc){
+        View view = View.inflate(this,R.layout.mark_image, null);//将要显示的Mark视图
+        MarkerOptions markerOption = new MarkerOptions() //自定义Mark
                 .icon(BitmapDescriptorFactory.fromView(view))//不支持SVG画出的图片
                 .draggable(false)
-                .position(latLng)
-                .title("当前位置")
-                .snippet("江苏省 南京市 建邺区")
+                .position(new LatLng(latitude,longitude))
+                .title(title)
+                .snippet(desc)
                 .setFlat(false);
-        //添加自定义Mark
-        Marker marker = aMap.addMarker(markerOption);
-        //自定义mark动画
-        Animation animation = new ScaleAnimation(0,1,0,1);
+        Marker marker = aMap.addMarker(markerOption);//添加自定义Mark
+        addMarkAnim(marker);
+        addMarkListener();
+        customInfoWindow();
+        return marker;
+    }
+
+    /**
+     * 为Mark地标添加动画
+     */
+    private void addMarkAnim(Marker marker){
+        Animation animation = new ScaleAnimation(2,1,2,1);//自定义mark动画
         animation.setDuration(1000);
         animation.setInterpolator(new BounceInterpolator());
         marker.setAnimation(animation);
         marker.startAnimation();
-
-
     }
 
+    /**
+     * 设置Mark地标点击监听
+     */
+    private void addMarkListener(){
+        //设置Mark点击事件
+        AMap.OnMarkerClickListener markListener = new AMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                if(!marker.isInfoWindowShown()){
+                    marker.showInfoWindow();
+                    Toast.makeText(MainActivity.this,
+                            "经度："
+                            + marker.getPosition().latitude
+                            + "纬度："
+                            + marker.getPosition().longitude,Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    marker.hideInfoWindow();
+                }
+                /**
+                 * 返回true则冒泡事件不向下传递，返回false则冒泡向下传递
+                 */
+                return true;
+            }
+        };
+        aMap.setOnMarkerClickListener(markListener);
+    }
 
+    /**
+     * 自定义infoWindow
+     */
+    private void customInfoWindow(){
+        aMap.setInfoWindowAdapter(new AMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                View view = LayoutInflater.from(MainActivity.this).inflate(
+                        R.layout.custom_info_window, null);
+                TextView title = view.findViewById(R.id.title);
+                TextView desc = view.findViewById(R.id.desc);
+                title.setText(marker.getTitle());
+                desc.setText(marker.getSnippet());
+                return view;
+            }
+            @Override
+            public View getInfoContents(Marker marker) {
+                return null;
+            }
+        });
+    }
 
     @Override
     protected void onDestroy() {
